d13ef138454b7d2460a7b5e80f49bfb91bf42a85
Oakley Hill <3017582h@student.gla.ac.uk>
2026-02-13 12:48:23 +0000

    chapter 10 done

--- rango/views.py
+++ rango/views.py
@@ -6,9 +6,27 @@ from django.urls import reverse
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
-from rango.form import CategoryForm
-from rango.form import PageForm
-from rango.form import UserForm,UserProfileForm
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from rango.forms import UserForm,UserProfileForm
+from datetime import datetime
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request,'visits','1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now)
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
 
 def index(request):
     cat_list = Category.objects.order_by('-likes')[:5]
@@ -18,7 +36,11 @@ def index(request):
     context_dict['boldmessage']='Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories']=cat_list
     context_dict['pages']=page_list
-    return render(request, 'rango/index.html', context = context_dict)
+
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context = context_dict)
+    return response
 
 def show_category(request,category_name_slug):
     context_dict={}
@@ -77,7 +99,12 @@ def add_page(request,category_name_slug):
 
 def about(request):
     context_dict = {'message': 'This tutorial was brought to you by Oakley Hill'}
-    return render(request, 'rango/about.html', context = context_dict)
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context = context_dict)
+    return response
 
 def register(request):
     registered = False
