e7b86200904d81cb362f2a83ae412ecfba37d0ec
Oakley Hill <3017582h@student.gla.ac.uk>
2026-02-13 13:57:20 +0000

    5.1

--- populate_rango.py
+++ populate_rango.py
@@ -8,7 +8,7 @@ from rango.models import Category, Page
 def populate():
     python_pages=[
         {'title':'Official Python Tutorial','url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How To Think Like A Computer Scientist','url':'http://www.greenteapress.com/thinkpython/'},
+        {'title':'How to Think like a Computer Scientist','url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes','url':'http://www.korokithakis.net/tutorials/python/'} ]
 
     django_pages=[{'title':'Official Django Tutorial','url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
